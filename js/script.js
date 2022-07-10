6df5eea768660d1ff5e75a970c91bb3e38723711
Julian Lescano <lescano.julian22@gmail.com>
2022-07-10 13:01:24 -0300

    repo created

---
+++ js/script.js
@@ -0,0 +1,21 @@
+
+let menu = document.getElementById("menu__hamburger");
+
+
+function myFunction(x) {
+  menu.classList.toggle("change-menu__hamburger");
+  x.classList.toggle("change");
+}
+
+
+function hamburger() {
+    let botonera =  document.getElementById('menu_dropdown')
+    if(botonera.style.display=='block') {
+    	botonera.style.display= 'none'
+    } else {
+    	botonera.style.display= 'block'
+    }
+}
+   
+
+
